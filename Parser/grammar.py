34960b0a283c51309495a72912b60a4072003fb7
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2024-06-05 21:25:09 +0330

    Update grammar and ast

--- Parser/grammar.py
+++ Parser/grammar.py
@@ -1,6 +1,16 @@
 import ply.yacc as yacc
+
 from Lexer.tokens import Tokens
 from Utils.ast import *
+
+
+class FilePosition(object):
+    def __init__(self, line):
+        self.line = line
+
+
+def getPosition(p):
+    return FilePosition(p.lexer.lexer.lineno - 1)
 
 
 class Parser:
@@ -15,10 +25,10 @@ class Parser:
         return self.parser.parse(data, lexer=lexer.lexer)
 
     def p_prog(self, p):
-        """prog : empty 
-                | func prog"""
+        """prog : empty
+        | func prog"""
         if len(p) == 3:
-            #todo
+            # todo
             p[0] = Program(prog=p[2], func=p[1], position=p.lineno(1))
 
     def p_empty(self, p):
@@ -28,21 +38,21 @@ class Parser:
 
     def p_stmt(self, p):
         """stmt : expr SEMI_COLON
-                | defvar SEMI_COLON
-                | func SEMI_COLON
-                | single_if
-                | else_if
-                | while_loop
-                | for_loop
-                | do_while
-                | return_is SEMI_COLON
-                | block"""
-        
+        | defvar SEMI_COLON
+        | func SEMI_COLON
+        | single_if
+        | else_if
+        | while_loop
+        | for_loop
+        | do_while
+        | return_is SEMI_COLON
+        | block"""
+
         p[0] = p[1]
 
     def p_body(self, p):
         """body : empty
-            | stmt body"""
+        | stmt body"""
         if len(p) == 3:
             p[0] = Body(statement=p[1], body=p[2])
 
@@ -52,16 +62,25 @@ class Parser:
 
     def p_while_loop(self, p):
         """while_loop : WHILE LPAREN expr RPAREN stmt"""
-        p[0] = WhileInstruction(condition=p[3], while_statement=p[5], position=p.lineno(1))
+        p[0] = WhileInstruction(
+            condition=p[3], while_statement=p[5], position=p.lineno(1)
+        )
 
     def p_for_loop(self, p):
         """for_loop : FOR LPAREN ID EQ expr TO expr RPAREN stmt"""
-        p[0] = ForInstruction(id=p[3], start_expr=p[5], end_expr=p[7], for_statement=p[9],
-                                position=p.lineno(1))
+        p[0] = ForInstruction(
+            id=p[3],
+            start_expr=p[5],
+            end_expr=p[7],
+            for_statement=p[9],
+            position=p.lineno(1),
+        )
 
     def p_do_while(self, p):
         """do_while : DO stmt WHILE DOUBLE_LSQUAREBR expr DOUBLE_RSQUAREBR"""
-        p[0] = WhileInstruction(condition=p[3], while_statement=p[5], position=p.lineno(1))
+        p[0] = WhileInstruction(
+            condition=p[3], while_statement=p[5], position=p.lineno(1)
+        )
 
     def p_block(self, p):
         """block : BEGIN body END"""
@@ -69,49 +88,59 @@ class Parser:
 
     def p_single_if(self, p):
         """single_if : IF DOUBLE_LSQUAREBR  expr DOUBLE_RSQUAREBR stmt"""
-        p[0] = IfOrIfElseInstruction(cond=p[3], if_statement=p[5], position=p.lineno(1), else_statement=None)
+        p[0] = IfOrIfElseInstruction(
+            cond=p[3], if_statement=p[5], position=p.lineno(1), else_statement=None
+        )
 
     def p_else_if(self, p):
         """else_if : IF DOUBLE_LSQUAREBR expr DOUBLE_RSQUAREBR stmt ELSE stmt"""
-        p[0] = IfOrIfElseInstruction(cond=p[3], if_statement=p[5], pos=p.lineno(1), else_statement=p[7])
+        p[0] = IfOrIfElseInstruction(
+            cond=p[3], if_statement=p[5], pos=p.lineno(1), else_statement=p[7]
+        )
 
     def p_defvar(self, p):
         """defvar : ID DBL_COLON type
-                    | ID DBL_COLON type EQ expr"""
+        | ID DBL_COLON type EQ expr"""
         if len(p) == 4:
             p[0] = VariableDecl(id=p[1], type=p[3], pos=p.lineno(1), expr=None)
         elif len(p) == 6:
             p[0] = VariableDecl(id=p[1], type=p[3], pos=p.lineno(1), expr=p[5])
-        
+
     def p_type(self, p):
         """type : INT
-                | STRING
-                | VECTOR
-                | NULL
-                | BOOLEAN"""
+        | STRING
+        | VECTOR
+        | NULL
+        | BOOLEAN"""
         p[0] = p[1]
 
     def p_func1(self, p):
         """func : FN ID LPAREN flist RPAREN LESS_THAN type GREATER_THAN LCURLYEBR body RCURLYEBR"""
-        p[0] = FunctionDef(rettype=p[7], name=p[2], params=p[4], body=p[10], pos=p.lineno(1))
+        p[0] = FunctionDef(
+            rettype=p[7], name=p[2], params=p[4], body=p[10], pos=p.lineno(1)
+        )
 
     def p_func2(self, p):
         """func :  FN ID LPAREN flist RPAREN LESS_THAN type GREATER_THAN EQ GREATER_THAN return_is"""
-        p[0] = FunctionDef(rettype=p[7], name=p[2], params=p[4], return_fn=p[11], pos=p.lineno(1))
+        p[0] = FunctionDef(
+            rettype=p[7], name=p[2], params=p[4], return_fn=p[11], pos=p.lineno(1)
+        )
 
     def p_flist(self, p):
         """flist : empty
-                | ID AS type
-                | ID  AS type COMMA flist"""
+        | ID AS type
+        | ID  AS type COMMA flist"""
         if len(p) == 3:
             p[0] = ParametersList(parameters=[Parameter(type=p[3], id=p[1])])
         if len(p) == 5:
-            p[0] = ParametersList(parameters=p[5].parameters + [Parameter(type=p[3], id=p[1])])
-        
+            p[0] = ParametersList(
+                parameters=p[5].parameters + [Parameter(type=p[3], id=p[1])]
+            )
+
     def p_clist(self, p):
         """clist : empty
-                | expr
-                | expr COMMA clist"""
+        | expr
+        | expr COMMA clist"""
         if len(p) == 2:
             if p[1] == []:
                 exprs = []
@@ -121,53 +150,80 @@ class Parser:
         elif len(p) == 4:
             p[0] = ExprList(exprs=p[3].exprs + [p[1]])
 
-    
     def p_expr(self, p):
-        """expr : expr LSQUAREBR expr RSQUAREBR
-                | LSQUAREBR clist RSQUAREBR
-                | expr QUESTION_MARK expr COLON expr
-                | expr PLUS expr
-                | expr MINUS expr
-                | expr TIMES expr
-                | expr DIVIDE expr
-                | expr GREATER_THAN expr
-                | expr LESS_THAN expr
-                | expr DOUBLE_EQ expr
-                | expr GREATER_THAN_EQ expr
-                | expr LESS_THAN_EQ expr
-                | expr NOT_EQ expr
-                | expr AND expr
-                | expr OR expr
-                | NOT expr
-                | PLUS expr
-                | MINUS expr
-                | ID
-                | ID EQ expr
-                | ID LPAREN clist RPAREN
-                | NUMBER
-                | STRING"""
+        """expr : on_list
+        | expr_list
+        | ternary_expr
+        | binary_expr
+        | ID
+        | assignment
+        | function_call
+        | NUMBER
+        | STRING"""
         if len(p) == 4 or len(p) == 3:
-            if p[1] == '-':
+            if p[1] == "-":
                 p[2].value = -p[2].value
-            elif p[1] == '!':
+            elif p[1] == "!":
                 if p[2].value:
                     p[2].value = 0
                 else:
                     p[2].value = 1
             p[0] = p[2]
         else:
-            if p.slice[1].type in ('number', 'iden', 'string'):
+            if p.slice[1].type in ("number", "iden", "string"):
                 p[0] = p.slice[1]
             else:
                 p[0] = p[1]
 
+    def p_assignment_expr(self, p):
+        """assignment : ID EQ expr"""
+        p[0] = Assignment(id=p[1], expr=p[3], pos=p.lineno(1))
 
-def p_expr_assignment(self, p):
-    """assignment :  iden equal expr"""
-    p[0] = Assignment(id=p[1], expr=p[3], pos=p.lineno(1))
+    def p_on_list(self, p):
+        """p_on_list : expr LSQUAREBR expr RSQUAREBR"""
+        p[0] = OperationOnList(expr=p[1], index_expr=p[3], pos=p.lineno(1))
 
-    
-        
+    def p_expr_list(self, p):
+        """expr_list : LSQUAREBR clist RSQUAREBR"""
+        p[0] = p[2]
+
+    def p_ternary_expr(self, p):
+        """ternary_expr : expr QUESTION_MARK expr COLON expr"""
+        p[0] = TernaryExpr(
+            cond=p[1], first_expr=p[2], second_expr=p[3], pos=p.lineno(1) + 1
+        )
+
+    def p_function_call(self, p):
+        """function_call : ID LPAREN clist RPAREN"""
+        p[0] = FunctionCall(id=p[1], args=p[3], pos=p.lineno(1) + 1)
+
+    def p_binary_expr(self, p):
+        """binary_expr :  expr PLUS expr
+        | expr MINUS expr
+        | expr TIMES expr
+        | expr DIVIDE expr
+        | expr GREATER_THAN expr
+        | expr LESS_THAN expr
+        | expr DOUBLE_EQ expr
+        | expr GREATER_THAN_EQ expr
+        | expr LESS_THAN_EQ expr
+        | expr NOT_EQ expr
+        | expr AND expr
+        | expr OR expr
+        | NOT expr
+        | PLUS expr
+        | MINUS expr"""
+        p[0] = BinExpr(left=p[1], op=p[2], right=p[3], pos=getPosition(p))
+
+    def p_builtin_methods(self, p):
+        """builtin_methods : SCAN LPAREN RPAREN
+                        | PRINT LPAREN clist RPAREN
+                        | LENGTH LPAREN clist RPAREN
+                        | EXIT LPAREN clist RPAREN"""
+        if len(p) == 4:
+            p[0] = FunctionCall(id=p[1], args=None, pos=p.lineno(1) + 1)
+        else:
+            p[0] = FunctionCall(id=p[1], args=p[3], pos=p.lineno(1) + 1)
     # def find_column(self, token):
     #     last_cr = self.lexer.lexer.lexdata.rfind('\n', 0, token.lexpos)
     #     if last_cr < 0:
--- Utils/ast.py
+++ Utils/ast.py
@@ -15,16 +15,19 @@ class Body(Node):
         self.statement = statement
         self.body = body
 
+
 class ReturnInstruction(Node):
     def __init__(self, expression, position):
         super().__init__(position)
         self.expression = expression
+
 
 class WhileInstruction(Node):
     def __init__(self, condition, while_statement, position):
         super().__init__(position)
         self.condition = condition
         self.while_statement = while_statement
+
 
 class ForInstruction(Node):
     def __init__(self, id, start_expr, end_expr, for_statement, position):
@@ -34,11 +37,13 @@ class ForInstruction(Node):
         self.end_expr = end_expr
         self.for_statement = for_statement
 
+
 class DoWhileInstruction(Node):
     def __init__(self, while_statement, condition, position):
         super().__init__(position)
         self.condition = condition
         self.while_statement = while_statement
+
 
 class Block(Node):
     def __init__(self, body):
@@ -52,6 +57,7 @@ class IfOrIfElseInstruction(Node):
         self.if_statement = if_statement
         self.else_statement = else_statement
 
+
 class VariableDecl(Node):
     def __init__(self, id, type, position, expr=None):
         super().__init__(position)
@@ -59,32 +65,66 @@ class VariableDecl(Node):
         self.type = type
         self.expr = expr
 
+
 class FunctionDef(Node):
     def __init__(self, rettype, name, params, position, body=None, return_fn=None):
-        super().__init__(position)        
+        super().__init__(position)
         self.rettype = rettype
         self.name = name
         self.fmlparams = params
         self.body = body
         self.return_fn = return_fn
 
+
 class ParametersList(Node):
     def __init__(self, parameters):
         self.parameters = parameters
+
     def __str__(self) -> str:
         return str(self.parameters)
-    
+
+
 class Parameter(Node):
     def __init__(self, type, id):
         self.type = type
         self.id = id
 
+
 class ExprList(Node):
     def __init__(self, exprs):
         self.exprs = exprs
+
 
 class Assignment(Node):
     def __init__(self, id, expr, pos):
         self.id = id
         self.expr = expr
         self.pos = pos
+
+
+class OperationOnList(Node):
+    def __init__(self, expr, index_expr, pos):
+        self.expr = expr
+        self.index_expr = index_expr
+        self.pos = pos
+
+
+class TernaryExpr(Node):
+    def __init__(self, cond, first_expr, second_expr, pos):
+        self.cond = cond
+        self.first_expr = first_expr
+        self.second_expr = second_expr
+        self.pos = pos
+
+
+class FunctionCall(Node):
+    def __init__(self, id, args, pos):
+        self.id = id
+        self.args = args
+        self.pos = pos
+
+class BinExpr(Node):
+    def __init__(self, left, op, right, pos):
+        self.left = left
+        self.op = op
+        self.right = right
